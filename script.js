88fc85edc97a3b6aa1c84cbb87b6e8990edcbf7f
Ilya Pavlov <pavlovgroups.ya.ru>
2020-11-15 13:39:29 +0300

    items-card

---
+++ script.js
@@ -0,0 +1,56 @@
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function() {
+
+  let select = function () {
+    let selectHeader = document.querySelectorAll('.select__header');
+    let selectItem = document.querySelectorAll('.select__item');
+
+    selectHeader.forEach(item => {
+        item.addEventListener('click', selectToggle)
+    });
+
+    selectItem.forEach(item => {
+        item.addEventListener('click', selectChoose)
+    });
+
+    function selectToggle() {
+        this.parentElement.classList.toggle('is-active');
+        this.querySelector('.select__icon').classList.toggle('active');
+    }
+
+    function selectChoose() {
+        let text = this.innerHTML,
+        select = this.closest('.select'),
+        currentText = select.querySelector('.select__current');
+        currentText.innerHTML = text;
+        select.classList.remove('is-active');
+        select.querySelector('.select__icon').classList.toggle('active');
+    }
+
+  };
+
+
+  select();
+
+  
+  
+  let qBtn = document.querySelectorAll('.item-windows__info-btn')
+
+  qBtn.forEach(function(elem) {
+    elem.addEventListener('click', function(){
+      let parent = this.closest('.item-windows')
+      parent.querySelector('.item-windows__price').classList.toggle('active')
+      parent.querySelector('.item-windows__material').classList.toggle('active')
+      parent.querySelector('.item-windows__info-btn').classList.toggle('active')
+      document.querySelector('.windows__grid').classList.toggle('active')
+      parent.classList.toggle('active')
+    })
+  })
+
+  
+    
+  
+
+
+})
